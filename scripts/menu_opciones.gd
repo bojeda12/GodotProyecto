235a2bd769bc1de09bf73c0c52b4f375842d5083
bojeda <brandon05251997@gmail.com>
2026-02-28 16:58:56 -0700

    Se agrego el splash y el menu de opciones

---
+++ scripts/menu_opciones.gd
@@ -0,0 +1,22 @@
+extends Control
+
+var menu_principal = "res://scenes/menu_inicio.tscn" 
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_atras_pressed() -> void:
+	get_tree().change_scene_to_file(menu_principal) # ruta del nodo control
+
+
+func _on_check_box_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
